4919726d5045029b2fd07af65098ad90df110188
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-03-05 09:07:18 +0530

    updating tags

--- modelUpload.py
+++ modelUpload.py
@@ -43,7 +43,7 @@ directory = "demotestmlrepo"
 fileName = "/SampleModelGeneratorScriptCopy.ipynb"
 filePath = modelUploadBase + directory + fileName
 
-tags = "age prediction, prediction"
+tags = "price prediction, prediction"
 
 metrics = getMetrics();
 
